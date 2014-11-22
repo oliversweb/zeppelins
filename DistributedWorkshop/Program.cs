3605aeaad5813191ed115c05c977ed85835fac61
henry.keen <henry.keen@just-eat.com>
2014-11-22 08:04:11 +0000

    Hello server/client

---
+++ DistributedWorkshop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetMQ;
+
+namespace DistributedWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NetMQContext ctx = NetMQContext.Create())
+            {
+                using (var server = ctx.CreateResponseSocket())
+                {
+                    server.Bind("tcp://192.168.43.121:5556");
+
+                    using (var client = ctx.CreateRequestSocket())
+                    {
+                        client.Connect("tcp://192.168.43.23:5556");
+                        client.Send("Hello");
+
+                        string m1 = server.ReceiveString();
+                        Console.WriteLine("From Client: {0}", m1);
+                        server.Send("Hi Back");
+
+                        string m2 = client.ReceiveString();
+                        Console.WriteLine("From Server: {0}", m2);
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
